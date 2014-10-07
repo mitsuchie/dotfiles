4775dbd65b321a48c79ba17bf38e376ee5d86389
mitsuchie <fsakura01@gmail.com>
2014-10-07 20:41:41 +0900

    Update viminstaller.sh

--- viminstaller.sh
+++ viminstaller.sh
@@ -1,28 +1,30 @@
-apt-get update && apt-get upgrade
+apt-get update -y
+apt-get upgrade -y
 apt-get install git -y
 apt-get install make -y
 
 # vim
 # apt-get build-dep vim -y
+
 apt-get install mercurial gettext libncurses5-dev libacl1-dev libgpm-dev -y
 apt-get install libperl-dev python-dev python3-dev ruby-dev -y
 apt-get install lua5.2 liblua5.2-dev -y
 apt-get install luajit libluajit-5.1 -y
 
 hg clone https://vim.googlecode.com/hg/ vimbuild
-cd /vimbuild \
-   && hg pull \
-   && hg update \
-   && ./configure --with-features=huge --enable-gui=gnome2 \
-                  --enable-perlinterp --enable-pythoninterp \
-                  --enable-python3interp --enable-rubyinterp \
-                  --enable-luainterp --with-luajit \
-                  --enable-fail-if-missing \
-                  --prefix=/usr/local \
-    && make \
-    && sudo make install
+cd vimbuild \
+  && hg pull \
+  && hg update \
+  && ./configure --with-features=huge --enable-gui=gnome2 \
+                 --enable-perlinterp --enable-pythoninterp \
+                 --enable-python3interp --enable-rubyinterp \
+                 --enable-luainterp --with-luajit \
+                 --enable-fail-if-missing \
+                 --prefix=/usr/local \
+   && make \
+   && sudo make install
 
-cd /
-rm -r /vimbuild
+cd ..
+rm -r vimbuild
 
 apt-get autoremove
