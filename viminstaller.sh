ceb94bc74d26c8b2571566958b481125f8caef32
mitsuchie <fsakura01@gmail.com>
2014-10-08 23:13:04 +0900

    Update viminstaller.sh

--- viminstaller.sh
+++ viminstaller.sh
@@ -1,14 +1,14 @@
-apt-get update -y
-apt-get upgrade -y
-apt-get install git make curl -y
+sudo apt-get update -y
+sudo apt-get upgrade -y
+sudo apt-get install git make curl -y
 
 # vim
 # apt-get build-dep vim -y
 
-apt-get install mercurial gettext libncurses5-dev libacl1-dev libgpm-dev -y
-apt-get install libperl-dev python-dev python3-dev ruby-dev -y
-apt-get install lua5.2 liblua5.2-dev -y
-apt-get install luajit libluajit-5.1 -y
+sudo apt-get install mercurial gettext libncurses5-dev libacl1-dev libgpm-dev -y
+sudo apt-get install libperl-dev python-dev python3-dev ruby-dev -y
+sudo apt-get install lua5.2 liblua5.2-dev -y
+sudo apt-get install luajit libluajit-5.1 -y
 
 hg clone https://vim.googlecode.com/hg/ vimbuild
 cd vimbuild \
@@ -20,13 +20,13 @@ cd vimbuild \
                  --enable-luainterp --with-luajit \
                  --enable-fail-if-missing \
                  --prefix=/usr/local \
-   && make \
+   && sudo make \
    && sudo make install
 
 cd ..
 rm -r vimbuild
 
-apt-get autoremove
+sudo apt-get autoremove
 
 curl https://raw.githubusercontent.com/Shougo/neobundle.vim/master/bin/install.sh | sh
-wget https://raw.githubusercontent.com/mitsuchie/vim/master/_vimrc ~/.vimrc
+wget https://raw.githubusercontent.com/mitsuchie/vim/master/_vimrc && mv _vimrc ~/.vimrc
