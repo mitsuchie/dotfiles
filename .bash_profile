447d2f7415dbc4fc0e49878c736581ffbf6798b4
mitsuchie <fsakura01@gmail.com>
2015-01-01 04:07:11 +0900

    fit to msys

---
+++ .bash_profile
@@ -0,0 +1,3 @@
+PS1="\[\e[0;33m\]X / _ / X <\[\e[0;37m\] "
+alias ls='ls --color=auto'
+alias ll='ls --color=auto -l'
---
+++ rubysetup.bat
@@ -0,0 +1,5 @@
+gem install rubocop --no-ri --no-rdoc
+gem install refe2   --no-ri --no-rdoc
+copy %~dp0bitclust/config %~dp0../.bitclust/config
+bitclust setup
+pause
