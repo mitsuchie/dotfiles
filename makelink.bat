a3ba57b5bf20b39cd1e3b06637afc7c14bf4b670
mitsuchie <fsakura01@gmail.com>
2015-02-05 01:15:46 +0900

    deal with ruby/rubocop

--- makelink.bat
+++ makelink.bat
@@ -1,4 +1,7 @@
-rem mklink "%USERPROFILE%/_gvimrc" "%~dp0_gvimrc" 
-rem mklink "%USERPROFILE%/_vimrc"  "%~dp0_vimrc"
-mklink "%USERPROFILE%/.gitconfig"  "%~dp0.gitconfig"
+mklink "%USERPROFILE%/.gitconfig"     "%~dp0.gitconfig"
+mklink "%USERPROFILE%/.bash_profile"  "%~dp0.bash_profile"
+mklink "%USERPROFILE%/.minttyrc"      "%~dp0.minttyrc"
+mklink "%USERPROFILE%/.gemrc"         "%~dp0.gemrc"
+mklink "%USERPROFILE%/.rubocop.yml"   "%~dp0.rubocop.yml"
+mklink "%USERPROFILE%/contestapplet.conf" "%~dp0contestapplet.conf"
 pause
--- makelink.sh
+++ makelink.sh
@@ -1,3 +1,4 @@
 # ln -s _gvimrc ~/.gvimrc
 # ln -s _vimrc  ~/.vimrc
 ln -s .gitconfig ~/.gitconfig
+ln -s .bash_profile ~/.bash_profile
