bda58402438fda0e4c9d0deedaf1ae0bdc655254
mitsuchie <fsakura01@gmail.com>
2020-04-25 22:54:52 +0900

    add zshrc

--- makelink.sh
+++ makelink.sh
@@ -2,6 +2,7 @@
 # ln -s _vimrc  ~/.vimrc
 ln -s ~/dotfiles/.gitconfig     ~/.gitconfig
 ln -s ~/dotfiles/.bash_profile  ~/.bash_profile
+ln -s ~/dotfiles/.zshrc         ~/.zshrc
 ln -s ~/dotflies/.minttyrc      ~/.minttyrc
 ln -s ~/dotfiles/.gemrc         ~/.gemrc
 ln -s ~/dotfiles/.rubocop.yml   ~/.rubocop.yml
